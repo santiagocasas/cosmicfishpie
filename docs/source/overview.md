886113144692813dcc4a4fb98b896231874c6e3b
Sefa Pamuk <sefa76q@hotmail.de>
2024-09-12 17:35:50 +0200

    started overview, added tests

--- docs/source/overview.md
+++ docs/source/overview.md
@@ -1,3 +1,34 @@
-Overview
-========
+# Cosmicfishpie
+
+## A Comprehensive Tool for Fisher Forecasts in Cosmology
+
+### Introduction
+
+`Cosmcfishpie` is a cosmological code to help researchers prepare for upcoming missions like Euclid, DESI, or SKAO. It contains a vast tool set to  produce Fisher forecasts for different applications. This code offers you easy access to cosmological quantities from leading Einstein-Boltzmann solvers `Class` and `CAMB` or the ability to directly read precomputed cosmologies from files.
+
+`Cosmicfishpie` allows the user to compute different cosmological observables like 3D power spectra of galaxies, angular power spectra of the cosmic microwave background, galaxy clustering or cosmic shear measurements. The code provides flexibility with different finite distance differentiation schemes for calculating the Fisher information matrix.
+
+Additionally, `Cosmicfishpie` excels in analysis and visualization. For this users can fix parameters in retrospect, combine information from independent experiments, and resample the results in new parameter bases. The code also allows comparisons of constraints across different surveys or settings, generating high-quality plots to effectively communicate findings.
+
+Whether you're preparing for future cosmological surveys or analyzing existing data, Cosmicfish offers a comprehensive and user-friendly approach to cosmological forecasting. In the following we will present a quick users guide to some of these functionalities.
+
+### Getting Started
+
+To install `cosmicfishpie` please refer to our [installation page](https://cosmicfishpie.readthedocs.io/en/latest/installation.html)
+You can install the code using `pip`
+
+~~~
+$ pip install cosmicfishpie
+~~~
+
+Any additional requirements will be installed and can be found in the requirements files.
+
+You can test your installation by running the `pytest` pipeline. For this please make sure that you have pytest installed and then simply run it in your console
+
+~~~
+$ pytest
+~~~
+
+This should compute a two-parameter Fisher and create a corner plot that you can check out in your results or plots folder.
+
 
---
+++ tests/installation_test.py
@@ -0,0 +1,55 @@
+import cosmicfishpie.fishermatrix.cosmicfish as cff
+import cosmicfishpie.analysis.fisher_plotting as cfp
+
+def test_installation():
+    # These are typical options that you can pass to Cosmicfishpie
+    options = {'accuracy': 1,
+            'outroot': 'test_installation_test_EBS',
+            'results_dir': 'results/',
+            'derivatives': '3PT',
+            'nonlinear': True,
+            'feedback': 2,
+            'specs_dir' : 'survey_specifications/',
+            'survey_name': 'Euclid',
+            'survey_name_spectro': 'Euclid-Spectroscopic-ISTF-Pessimistic',
+            'survey_name_photo': 'Euclid-Photometric-ISTF-Pessimistic',
+            'cosmo_model' : 'LCDM',
+            'code': 'camb',
+            'class_config_yaml':'../boltzmann_yaml_files/camb/default.yaml',
+            }
+
+    #Internally CosmicFish converts these parameters to the corresponding parameters in CAMB or CLASS 
+    fiducial = {"Omegam":0.32,
+                "h":0.67,
+    }
+    
+    #Parameters to be varied and analyzed and their percentage variation for numerical derivatives
+    freepars = {'Omegam': 0.01,
+                'h': 0.01,
+    }
+
+    observables = ['GCsp']
+
+    cosmoFM = cff.FisherMatrix(
+        fiducialpars=fiducial,
+        freepars=freepars,
+        options=options,
+        observables=observables,
+        cosmoModel=options['cosmo_model'],
+        surveyName=options['survey_name'])
+
+    FA = cosmoFM.compute()
+
+    plot_options = {'fishers_list': [FA], 
+                    'fish_labels': ['Euclid Spectroscopic pessimistic'],
+                    'plot_pars': list(freepars.keys()),
+                    'axis_custom_factors': {'all':7},  ## Axis limits cover 3-sigma bounds of first Fisher matrix
+                    'plot_method': 'Gaussian',
+                    'file_format': '.pdf',   ##file format for all the plots
+                    'outpath' : './plots/',  ## directory where to store the files, if non-existent, it will be created
+                    'outroot':'test_installation_test_plot',  ## file name root for all the plots, extra names can be added individually
+                    'colors':["#000000"],
+                    }
+
+    fish_plotter = cfp.fisher_plotting(**plot_options)
+    fish_plotter.plot_fisher(filled=[False])

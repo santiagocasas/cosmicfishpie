4a80d512af74d0e237a6c35b18e82e9087bb3f10
Santiago Casas <santiagocasas@gmail.com>
2024-12-01 23:18:16 +0100

    likelihood scripts

---
+++ notebooks/likelihod_photometric.py
@@ -0,0 +1,450 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import seaborn as sns
+import numpy as np
+import logging
+from itertools import product
+from copy import deepcopy, copy
+from collections.abc import Sequence
+
+
+from cosmicfishpie.fishermatrix import cosmicfish
+from cosmicfishpie.LSSsurvey import photo_obs as pobs
+from cosmicfishpie.LSSsurvey import photo_cov as pcov
+from cosmicfishpie.utilities.utils import printing as upr
+from nautilus import Prior
+from nautilus import Sampler
+import re
+import time
+
+
+snscolors = sns.color_palette("colorblind")
+def is_indexable_iterable(var):
+    return isinstance(var, (list, np.ndarray, Sequence)) and not isinstance(var, (str, bytes))
+
+
+logger = logging.getLogger("cosmicfishpie.cosmology.nuisance")
+logger.setLevel(logging.INFO)
+
+
+upr.debug = False
+
+
+upr.debug_print("test")
+
+
+outroot = "cosmicjellyfish_Euclid-ISTF-Pess-3x2photo_symb_withnuis"
+
+
+sampler_settings = {
+    "n_live": 2000,
+    "n_networks": 16,
+    "n_batch": 256,
+    "pool": 8,
+}
+
+
+fiducial = {
+    "Omegam": 0.3145714273,
+    "Omegab": 0.0491989,
+    "h": 0.6737,
+    "ns": 0.96605,
+    "sigma8": 0.81,
+    "w0": -1.0,
+    "wa": 0.0,
+    "mnu": 0.06,
+    "Neff": 3.044,
+}
+observables = ['WL', 'GCph']
+
+
+options = {
+    "accuracy": 1,
+    "feedback": 1,
+    "code": "symbolic",
+    "outroot": outroot,
+    "survey_name": "Euclid",
+    "survey_name_photo": "Euclid-Photometric-ISTF-Pessimistic",
+    "survey_name_spectro": False,
+    "specs_dir": "../cosmicfishpie/configs/default_survey_specifications/",
+    "cosmo_model": "LCDM",
+}
+cosmoFM_fid = cosmicfish.FisherMatrix(
+    fiducialpars=fiducial,
+    options=options,
+    observables=observables,
+    cosmoModel=options["cosmo_model"],
+    surveyName=options["survey_name"],
+)
+
+
+cosmoFM_fid.IApars
+
+
+photo_fid = pobs.ComputeCls(cosmopars=cosmoFM_fid.fiducialcosmopars,
+                            photopars=cosmoFM_fid.photopars,
+                            IApars=cosmoFM_fid.IApars,
+                            biaspars=cosmoFM_fid.photobiaspars)
+
+photo_fid.compute_all()
+
+photo_cov_fid = pcov.PhotoCov(cosmopars=cosmoFM_fid.fiducialcosmopars,
+    photopars=cosmoFM_fid.photopars,
+    IApars=cosmoFM_fid.IApars,
+    biaspars=cosmoFM_fid.photobiaspars,
+    fiducial_Cls=photo_fid)
+
+
+photo_cov_fid.allparsfid
+
+
+print(photo_fid.binrange_WL)
+print(photo_fid.binrange_GCph)
+print(photo_cov_fid.ngalbin_WL)
+print(photo_cov_fid.ngalbin_GCph)
+
+
+def observable_Cell(photo_th: pobs.ComputeCls):
+    photo_th.compute_all()
+    binrange_GCph = photo_th.binrange_GCph
+    binrange_WL = photo_th.binrange_WL
+    nbin_GCph = len(binrange_GCph)
+    nbin_WL = len(binrange_WL)
+
+    ells = photo_th.result["ells"]
+    output = dict(ells=ells)
+
+    observables = photo_th.observables
+    if "WL" in observables:
+        Cell_LL = np.empty((len(ells), nbin_WL, nbin_WL), dtype=np.float64)
+    if "GCph" in observables:
+        Cell_GG = np.empty((len(ells), nbin_GCph, nbin_GCph), dtype=np.float64)
+    if "WL" in observables and "GCph" in observables:
+        Cell_GL = np.empty((len(ells), nbin_GCph, nbin_WL), dtype=np.float64)
+
+    for i,j in product(binrange_WL, binrange_GCph):
+
+        if "WL" in observables:
+            Cell_LL[:,i-1,j-1] = (photo_th.result["WL {}xWL {}".format(i,j)]
+                                  + np.eye(nbin_WL)[i-1,j-1]
+                                  * photo_cov_fid.ellipt_error**2.0 / photo_cov_fid.ngalbin_WL[i-1]
+                                  )
+
+        if "GCph" in observables:
+            Cell_GG[:,i-1,j-1] = (photo_th.result["GCph {}xGCph {}".format(i,j)]
+                                  + np.eye(nbin_GCph)[i-1,j-1]
+                                  * 1 / photo_cov_fid.ngalbin_GCph[i-1]
+                                  )
+
+        if "WL" in observables and "GCph" in observables:
+            Cell_GL[:,i-1,j-1] = photo_th.result["GCph {}xWL {}".format(i,j)]
+
+    if "WL" in observables:
+        output["Cell_LL"] = Cell_LL
+    if "GCph" in observables:
+        output["Cell_GG"] = Cell_GG
+    if "WL" in observables and "GCph" in observables:
+        output["Cell_GL"] = Cell_GL
+
+    return output
+
+
+Cells_fid = observable_Cell(photo_fid)
+print(Cells_fid.keys())
+
+
+print(Cells_fid["Cell_LL"].shape)
+print(Cells_fid["Cell_GG"].shape)
+print(Cells_fid["Cell_GL"].shape)
+
+
+ellmax_WL = cosmoFM_fid.specs["lmax_WL"]
+ellmax_GC = cosmoFM_fid.specs["lmax_GCph"]
+ellmax_XC = np.minimum(ellmax_GC,ellmax_WL)
+nbins_Glob = min(len(list(photo_fid.binrange_WL)), len(list(photo_fid.binrange_GCph)))
+print(nbins_Glob)
+
+
+def compute_chi2_per_obs(Cell_fid, Cell_th, ells, dells):
+
+    dfid = np.linalg.det(Cell_fid)
+    dth = np.linalg.det(Cell_th)
+
+    nells = len(ells)
+    _, _, nbin = Cell_fid.shape
+
+    dmix = 0
+    for i in range(nbin):
+        Cth_mix = copy(Cell_th)
+        Cth_mix[:,i,:] = Cell_fid[:,i,:]
+        dmix += np.linalg.det(Cth_mix)
+
+    ingrd = (
+        (2*ells+1)
+        *(
+        dmix[:nells]/dth[:nells]
+        + np.log(dth[:nells]/dfid[:nells])
+        - nbin)
+    )
+    ingrd = [*((ingrd[1:]+ingrd[:-1])/2 * dells[:-1]), ingrd[-1]*dells[-1]]
+
+    chi2 = np.sum(ingrd)
+    return chi2
+
+
+def compute_chi2(Cells_fid, Cells_th):
+    """
+    Compute χ² for wedges using fully vectorized operations.
+    Matches the loop implementation exactly.
+
+    Parameters:
+    ----------
+    Cells_fid: Dict
+
+    Cells_th: Dict
+
+    Returns:
+    -------
+    float
+        χ² value
+    """
+    chi2 = 0
+    ells = Cells_fid["ells"]
+
+    if "WL" in observables and not "GCph" in observables:
+        Cells_WL_th = Cells_th["Cell_LL"]
+        Cells_WL_fid = Cells_fid["Cell_LL"]
+
+        iWL = np.searchsorted(ells, ellmax_WL)
+        ells_WL = np.insert(ells, iWL, ellmax_WL)
+        Dl_WL = np.diff(ells_WL)[:iWL]
+        ells_WL = ells_WL[:iWL]
+
+        
+        chi2 += (
+            photo_cov_fid.fsky_WL
+            * compute_chi2_per_obs(Cells_WL_fid, Cells_WL_th, ells_WL, Dl_WL)
+        )
+
+    if "GCph" in observables and not "WL" in observables:
+        Cells_GC_th = Cells_th["Cell_GG"]
+        Cells_GC_fid = Cells_fid["Cell_GG"]
+
+        iGC = np.searchsorted(ells, ellmax_GC)
+        ells_GC = np.insert(ells, iGC, ellmax_GC)
+        Dl_GC = np.diff(ells_GC)[:iGC]
+        ells_GC = ells_GC[:iGC]
+
+        chi2 += (photo_cov_fid.fsky_GCph
+                 * compute_chi2_per_obs(Cells_GC_fid, Cells_GC_th, ells_GC, Dl_GC))
+
+    if "GCph" in observables and "WL" in observables:
+        Cells_XC_th = Cells_th["Cell_GL"]
+        Cells_XC_fid = Cells_fid["Cell_GL"]
+        Cells_GC_th = Cells_th["Cell_GG"]
+        Cells_GC_fid = Cells_fid["Cell_GG"]
+        Cells_WL_th = Cells_th["Cell_LL"]
+        Cells_WL_fid = Cells_fid["Cell_LL"]
+
+        iGC = np.searchsorted(ells, ellmax_GC)
+        ells_GC = np.insert(ells, iGC, ellmax_GC)
+        Dl_GC = np.diff(ells_GC)[:iGC]
+        ells_GC = ells_GC[:iGC]
+        iWL = np.searchsorted(ells, ellmax_WL)
+        ells_WL = np.insert(ells, iWL, ellmax_WL)
+        Dl_WL = np.diff(ells_WL)[:iWL]
+        ells_WL = ells_WL[:iWL]
+        iXC = np.searchsorted(ells, ellmax_XC)
+        ells_XC = np.insert(ells, iXC, ellmax_XC)
+        Dl_XC = np.diff(ells_XC)[:iXC]
+        ells_XC = ells_GC[:iXC]
+
+        big_th = np.block([[Cells_WL_th[:iXC],np.transpose(Cells_XC_th,(0,2,1))[:iXC]],
+                           [Cells_XC_th[:iXC], Cells_GC_th[:iXC]]])
+        big_fid = np.block([[Cells_WL_fid[:iXC],np.transpose(Cells_XC_fid,(0,2,1))[:iXC]],
+                           [Cells_XC_fid[:iXC], Cells_GC_fid[:iXC]]])
+
+        chi2 += np.sqrt(photo_cov_fid.fsky_WL*photo_cov_fid.fsky_GCph) * compute_chi2_per_obs(big_fid, big_th,ells_XC, Dl_XC)
+        chi2 += photo_cov_fid.fsky_WL * compute_chi2_per_obs(Cells_WL_fid[:iXC], Cells_WL_th[:iXC],ells_WL[:iXC], Dl_WL[:iXC])
+
+    return chi2
+
+
+def loglike(param_vec, prior=None):
+    
+    if type(param_vec) == dict:
+        param_dict = deepcopy(param_vec)
+    elif is_indexable_iterable(param_vec) and prior is not None:
+        #print(f'Loading prior with keys: {prior.keys}')
+        param_dict={key: param_vec[i] for i, key in enumerate(prior.keys)}
+    
+    photopars = deepcopy(cosmoFM_fid.photopars)
+    for ii, pp in enumerate(cosmoFM_fid.photopars.keys()):
+        photopars[ii] = param_dict.pop(pp, cosmoFM_fid.photopars[pp])
+    
+    photobiaspars = deepcopy(cosmoFM_fid.photobiaspars)
+    for ii, pp in enumerate(cosmoFM_fid.photobiaspars.keys()):
+        photobiaspars[pp] = param_dict.pop(pp, cosmoFM_fid.photobiaspars[pp])
+    
+    IApars = deepcopy(cosmoFM_fid.IApars)
+    for ii, pp in enumerate(cosmoFM_fid.IApars.keys()):
+        IApars[pp] = param_dict.pop(pp, cosmoFM_fid.IApars[pp])
+    
+    photo_vary = pobs.ComputeCls(
+        param_dict,
+        photopars,
+        IApars,
+        photobiaspars,
+        )
+    Cells_th = observable_Cell(photo_vary)
+
+    return -0.5 * compute_chi2(Cells_fid,Cells_th)
+
+
+cosmoFM_fid.freeparams
+
+
+cosmoFM_fid.allparams
+
+
+samp1dic = {'Omegam': 0.3145714273,
+ 'Omegab': 0.0491989,
+ 'h': 0.6737,
+ 'ns': 0.96605,
+ 'sigma8': 0.81,
+ 'w0': -1.0,
+ 'wa': 0.0,
+ 'mnu': 0.06,
+ 'Neff': 3.044,
+ 'bias_model': 'binned',
+ 'b1': 1.0997727037892875,
+ 'b2': 1.220245876862528,
+ 'b3': 1.2723993083933989,
+ 'b4': 1.316624471897739,
+ 'b5': 1.35812370570578,
+ 'b6': 1.3998214171814918,
+ 'b7': 1.4446452851824907,
+ 'b8': 1.4964959071110084,
+ 'b9': 1.5652475842498528,
+ 'b10': 1.7429859437184225,
+ 'fout': 0.1,
+ 'co': 1,
+ 'cb': 1,
+ 'sigma_o': 0.05,
+ 'sigma_b': 0.05,
+ 'zo': 0.1,
+ 'zb': 0.0,
+ 'IA_model': 'eNLA',
+ 'AIA': 1.72,
+ 'betaIA': 2.17,
+ 'etaIA': -0.41*1.1}
+print("Sample likelihood", loglike(samp1dic))
+
+
+loglike(photo_cov_fid.allparsfid)
+
+
+photo_cov_fid.allparsfid
+
+
+fishmat_photo = cosmoFM_fid.compute()
+
+
+prior_nonuis = Prior()
+prior_withnuis = Prior()
+
+
+prior_dict ={
+ 'Omegam': [0.24, 0.4],
+ 'Omegab': [0.04, 0.06],
+ 'h': [0.61, 0.75],
+ 'ns': [0.92, 1.00],
+ 'sigma8': [0.79, 0.83],
+ 'AIA': [1.0, 3.0],
+ 'etaIA' :[-6.0, 6.0],
+ 'b1': [1.0, 3.0],
+ 'b2': [1.0, 3.0],
+ 'b3': [1.0, 3.0],
+ 'b4': [1.0, 3.0],
+ 'b5': [1.0, 3.0],
+ 'b6': [1.0, 3.0],
+ 'b7': [1.0, 3.0],
+ 'b8': [1.0, 3.0],
+ 'b9': [1.0, 3.0],
+ 'b10': [1.0, 3.0]
+ }
+
+
+cosmoFM_fid.freeparams
+
+
+for par in prior_dict.keys():
+    if par in cosmoFM_fid.freeparams.keys():
+        dist_prior = (prior_dict[par][0], prior_dict[par][1])
+        if re.match(r'b\d+', par):
+            prior_withnuis.add_parameter(par, dist_prior)
+        elif re.search(r'IA', par):
+            prior_withnuis.add_parameter(par, dist_prior)
+        else:
+            prior_nonuis.add_parameter(par, dist_prior)
+            prior_withnuis.add_parameter(par, dist_prior)
+
+
+print(prior_nonuis.keys)
+print(prior_withnuis.keys)
+
+
+if "withnuis" in options["outroot"]:
+    prior_chosen = prior_withnuis
+elif "nonuis" in options["outroot"]:
+    prior_chosen = prior_nonuis
+else:
+    raise ValueError("No prior specified in the outroot")
+print("Loading prior with keys: ", prior_chosen.keys)
+
+
+tini = time.time()
+print("Starting sampler at", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(tini)))
+
+
+sampler = Sampler(prior_chosen, 
+                          loglike, 
+                          n_live=sampler_settings["n_live"], 
+                          n_networks=sampler_settings["n_networks"], 
+                          n_batch=sampler_settings["n_batch"], 
+                          pool=sampler_settings["pool"], 
+                          pass_dict=False,
+                          filepath=options["outroot"]+".hdf5", 
+                          resume=True,
+                          likelihood_kwargs={'prior': prior_chosen}
+                          )
+sampler.run(verbose=True, discard_exploration=True)
+log_z_all = sampler.evidence()
+print('Evidence:', log_z_all)
+points_all, log_w_all, log_l_all = sampler.posterior()
+
+
+tfin = time.time()
+elapsed = tfin - tini
+hours = int(elapsed // 3600)
+minutes = int((elapsed % 3600) // 60)
+seconds = int(elapsed % 60)
+print("Sampler finished at", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(tfin)))
+print(f"Total time elapsed: {hours:02d}:{minutes:02d}:{seconds:02d}")
+
+
+sample_wghlkl = (np.vstack((points_all.T, np.exp(log_w_all), log_l_all)).T)
+
+
+outfile_chain = options["outroot"]+".txt"
+print(f"Saving chain to text file {outfile_chain}")
+
+
+headerlist = ['loglike', 'weights'] + list(prior_chosen.keys)
+header = " ".join(headerlist)
+print("Saving header: ", header)
+
+
+np.savetxt(outfile_chain, sample_wghlkl, header=header)
+
---
+++ notebooks/likelihod_spectroscopic.py
@@ -0,0 +1,424 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import logging
+from copy import deepcopy
+import os
+
+
+from cosmicfishpie.fishermatrix import cosmicfish
+from cosmicfishpie.LSSsurvey import spectro_obs as spobs
+from cosmicfishpie.LSSsurvey import spectro_cov as spcov
+from cosmicfishpie.utilities.utils import printing as upr
+from cosmicfishpie.analysis import fisher_operations as cfop
+
+
+from nautilus import Prior
+from nautilus import Sampler
+import warnings
+
+
+from cosmicfishpie.likelihood.spectro_like import observable_Pgg, legendre_Pgg, loglike
+
+
+os.environ["OMP_NUM_THREADS"] = "6"
+
+
+# # Compute fiducial data and theory
+
+# fiducial = {
+#     "Omegam": 0.3145714273,
+#     "Omegab": 0.0491989,
+#     "h": 0.6737,
+#     "ns": 0.96605,
+#     "sigma8": 0.81,
+#     "w0": -1.0,
+#     "wa": 0.0,
+#     "mnu": 0.06,
+#     "Neff": 3.044,
+# }
+
+fiducial = {
+        "Omegam": 0.3145714273,
+        "Omegab": 0.0491989,
+        "h": 0.6737,
+        "ns": 0.96605,
+        "sigma8": 0.81,
+            # "Omegab":0.04897,
+            # "h":0.6766,
+            # "ns":0.9665,
+            # "sigma8":0.815584,
+            #"As" : 2.1098301595078886e-09,
+            "w0":-1.0,
+            "wa":0.,
+            "mnu":0.0,
+            #"Neff":3.044,
+            "num_massive_neutrinos" : 0,
+            "num_nu_massive": 0,
+            "num_nu_massless": 3.044,
+            "tau": 0.0561,
+            'kmax': 10,
+            'extrap_kmax': None
+            }
+observables = ['GCsp']
+
+
+# ## Compute symbolic Observable
+
+options = {
+    "accuracy": 1,
+    "feedback": 1,
+    "code": "symbolic",
+    "outroot": "Euc-ISTF-Pess-symb",
+    "survey_name": "Euclid",
+    #"survey_name_spectro": "SKAO-Spectroscopic-Redbook",
+    "survey_name_spectro": "Euclid-Spectroscopic-ISTF-Pessimistic",
+    "survey_name_photo": False,
+    #"survey_name_radio_IM": "SKAO-IM-Redbook",
+    #'specs_dir': '../cosmicfishpie/configs/other_survey_specifications/',
+    "cosmo_model": "LCDM",
+    "bfs8terms": False,
+}
+cosmoFM_theory = cosmicfish.FisherMatrix(
+    fiducialpars=fiducial,
+    options=options,
+    observables=observables,
+    cosmoModel=options["cosmo_model"],
+    surveyName=options["survey_name"],
+)
+print(cosmoFM_theory.Spectrobiaspars)
+print(cosmoFM_theory.freeparams)
+theory_fish = cosmoFM_theory.compute()
+
+
+#cosmoFM_fid.set_pk_settings()
+print(cosmoFM_theory.pk_cov.global_z_bin_mids)
+print(cosmoFM_theory.Pk_kgrid.shape)
+print(cosmoFM_theory.obs_spectrum)
+print(cosmoFM_theory.Pk_musamp)
+print(cosmoFM_theory.Pk_ksamp)
+print(cosmoFM_theory.Pk_mumesh.shape)
+print(cosmoFM_theory.Pk_kgrid.shape)
+# spectro_fid = spobs.ComputeGalSpectro(cosmoFM_fid.fiducialcosmopars,
+#                                        spectrobiaspars=cosmoFM_fid.Spectrobiaspars,
+#                                        spectrononlinearpars=cosmoFM_fid.Spectrononlinpars,
+#                                        PShotpars=cosmoFM_fid.PShotpars,
+#                                        IMbiaspars=cosmoFM_fid.IMbiaspars,
+#                                        fiducial_cosmo=cosmoFM_fid.fiducialcosmo,
+#                                        use_bias_funcs=False, 
+#                                        configuration=cosmoFM_fid)
+
+# spectro_cov_fid = spcov.SpectroCov(fiducialpars=cosmoFM_fid.fiducialcosmopars,
+#                                    configuration=cosmoFM_fid)
+
+
+#volume_surveys = np.array([spectro_cov_fid.volume_survey(ii) for ii in range(len(spectro_cov_fid.global_z_bin_mids))])
+
+
+# ## Compute camb Observable
+
+options = {
+    "accuracy": 1,
+    "feedback": 1,
+    "code": "camb",
+    "outroot": "Euc-ISTF-Pess-camb",
+    "survey_name": "Euclid",
+    #"survey_name_spectro": "SKAO-Spectroscopic-Redbook",
+    "survey_name_spectro": "Euclid-Spectroscopic-ISTF-Pessimistic",
+    "survey_name_photo": False,
+    #"survey_name_radio_IM": "SKAO-IM-Redbook",
+    #'specs_dir': '../cosmicfishpie/configs/other_survey_specifications/',
+    "cosmo_model": "LCDM",
+    "bfs8terms": False,
+}
+cosmoFM_data = cosmicfish.FisherMatrix(
+    fiducialpars=fiducial,
+    options=options,
+    observables=observables,
+    cosmoModel=options["cosmo_model"],
+    surveyName=options["survey_name"],
+)
+print(cosmoFM_data.Spectrobiaspars)
+print(cosmoFM_data.freeparams)
+data_fish = cosmoFM_data.compute()
+
+
+#cosmoFM_fid.set_pk_settings()
+print(cosmoFM_data.pk_cov.global_z_bin_mids)
+print(cosmoFM_data.Pk_kgrid.shape)
+print(cosmoFM_data.obs_spectrum)
+print(cosmoFM_data.Pk_musamp)
+print(cosmoFM_data.Pk_ksamp)
+print(cosmoFM_data.Pk_mumesh.shape)
+print(cosmoFM_data.Pk_kgrid.shape)
+spectro_data = spobs.ComputeGalSpectro(cosmoFM_data.fiducialcosmopars,
+                                       spectrobiaspars=cosmoFM_data.Spectrobiaspars,
+                                       spectrononlinearpars=cosmoFM_data.Spectrononlinpars,
+                                       PShotpars=cosmoFM_data.PShotpars,
+                                       IMbiaspars=cosmoFM_data.IMbiaspars,
+                                       fiducial_cosmo=cosmoFM_data.fiducialcosmo,
+                                       use_bias_funcs=False, 
+                                       configuration=cosmoFM_data)
+
+spectro_cov_data = spcov.SpectroCov(fiducialpars=cosmoFM_data.fiducialcosmopars,
+                                   configuration=cosmoFM_data)
+
+
+
+min(cosmoFM_theory.Pk_kgrid)
+
+
+from matplotlib import pyplot as plt
+fig, axs = plt.subplots(2, 1, figsize=(10, 10))
+pktheo = cosmoFM_theory.pk_obs_fid.observed_Pgg(1.0, cosmoFM_theory.Pk_kgrid, 0.66)
+pkdata = cosmoFM_data.pk_obs_fid.observed_Pgg(1.0, cosmoFM_data.Pk_kgrid, 0.66)
+axs[0].loglog(cosmoFM_theory.Pk_kgrid, pktheo)
+axs[0].loglog(cosmoFM_data.Pk_kgrid, pkdata)
+axs[0].legend(['symbolicPk Theory', 'CAMB Data'])
+axs[1].semilogx(cosmoFM_theory.Pk_kgrid, (pktheo/pkdata - 1))
+axs[0].set_xlim(1e-3, 0.2)
+axs[1].set_xlim(1e-3, 0.2)
+axs[1].set_ylim(-0.01, 0.01)
+axs[0].set_ylabel(r'$P_{gg}(k,z, \mu)$')
+axs[1].set_ylabel(r'$P_{th}/P_{dat} - 1$')
+axs[1].set_xlabel(r'$k$ [1/Mpc]')
+axs[1].legend('Ratio Theory to Data')
+
+
+parss = theory_fish.get_param_names()[0:6]
+parss
+
+
+truth_fish = dict(zip(parss, theory_fish.get_param_fiducial()[0:6]))
+truth_fish
+
+
+parss_nonui = parss[0:5]
+parss_nonui
+
+
+theory_fish_nonui = cfop.reshuffle(theory_fish, names = parss_nonui)
+data_fish_nonui = cfop.reshuffle(data_fish, names = parss_nonui)
+
+
+import cosmicfishpie.analysis.fishconsumer as fco
+euclid_sp_all = fco.make_triangle_plot(
+    fishers=[
+        theory_fish, 
+        theory_fish_nonui, 
+        data_fish,
+        data_fish_nonui
+        ],
+    colors=[
+        'red', 
+        'orange',
+        'blue',
+        'green'
+        ],
+    fisher_labels=[
+        'Euclid Fisher GCsp symbolic',
+        'Euclid Fisher GCsp no-nui symbolic',
+        'Euclid Fisher GCsp camb',
+        'Euclid Fisher GCsp no-nui camb',
+        ],
+    truth_values=truth_fish,
+    params=['h', 'ns', 'sigma8']
+)
+
+
+# ## Choose data vector
+
+obsPgg_data = observable_Pgg(spectro_cov_data, cosmoFM_data)
+obsPgg_data.shape
+
+
+P_ell_data = legendre_Pgg(obsPgg_data, cosmoFM_data)
+P_ell_data.shape
+
+
+fid_truth = {par : cosmoFM_data.allparams[par] for par in cosmoFM_data.freeparams.keys()}
+fid_truth
+
+
+# ## Compute example loglike
+
+samp1dic = {'Omegam': 0.31,
+            'Omegab': 0.05,
+            'h': 0.68,
+            'ns': 0.96,
+            'sigma8': 0.82,
+            'w0': -1.01,
+            'wa': 0.2,
+            'Ps_0': 0.0,
+            'Ps_1': 0.0,
+            'Ps_2': 0.0,
+            'Ps_3': 10.0,
+            'lnbg_1': 0.37944989,
+            'lnbg_2': 0.4738057,
+            'lnbg_3': 0.55760176,
+            'lnbg_4': 0.6,
+            'sigmap_1': 1.0,
+            'sigmap_2': 0.,
+            'sigmap_3': 1.0,
+            'sigmap_4': 1.0,
+            'sigmav_1': 1.0,
+            'sigmav_2': 1.0,
+            'sigmav_3': 0.,
+            'sigmav_4': 10,
+            'bI_c1': -0.3,
+            'bI_c2': +0.6,
+            }
+print("Wedges loglike: ", loglike(samp1dic, leg_flag='wedges', cosmoFM_theory=cosmoFM_theory, cosmoFM_data=cosmoFM_data, data_obsPgg=obsPgg_data))
+print("Multipoles loglike: ", loglike(samp1dic, leg_flag='legendre', cosmoFM_theory=cosmoFM_theory, cosmoFM_data=cosmoFM_data, data_obsPgg=obsPgg_data))
+
+
+get_ipython().run_cell_magic('timeit', '', "loglike(samp1dic, leg_flag='wedges', cosmoFM_theory=cosmoFM_theory, cosmoFM_data=cosmoFM_data, data_obsPgg=obsPgg_data)\n")
+
+
+get_ipython().run_cell_magic('timeit', '', 'with warnings.catch_warnings():\n    warnings.filterwarnings("ignore")\n    loglike(samp1dic, leg_flag=\'legendre\', cosmoFM_theory=cosmoFM_theory, cosmoFM_data=cosmoFM_data, data_obsPgg=obsPgg_data)\n')
+
+
+# ## Initialize Nautilus sampler
+
+prior = Prior()
+prior_noshot = Prior()
+prior_noshot_no_bi = Prior()
+prior_nosigma = Prior()
+
+
+prior_dict ={
+ 'Omegam': [0.24, 0.4],
+ 'Omegab': [0.04, 0.06],
+ 'h': [0.61, 0.73],
+ 'ns': [0.92, 1.00],
+ 'sigma8': [0.79, 0.83],
+ 'Ps_0': [-10, 10],
+ 'Ps_1': [-10, 10],
+ 'Ps_2': [-10, 10],
+ 'Ps_3': [-10, 10],
+ 'lnbg_1': [0, 1],
+ 'lnbg_2': [0, 1],
+ 'lnbg_3': [0, 1],
+ 'lnbg_4': [0, 1],
+ 'sigmap_1': [0, 10],
+ 'sigmap_2': [0, 10],
+ 'sigmap_3': [0, 10],
+ 'sigmap_4': [0, 10],
+ 'sigmav_1': [0, 10],
+ 'sigmav_2': [0, 10],
+ 'sigmav_3': [0, 10],
+ 'sigmav_4': [0, 10],
+ 'bI_c1': [-5, 5],
+ 'bI_c2': [-5, 5],
+ }
+
+
+cosmoFM_theory.freeparams
+
+
+for par in prior_dict.keys():
+    if par in cosmoFM_theory.freeparams.keys():
+        dist_prior = (prior_dict[par][0], prior_dict[par][1])
+        prior.add_parameter(par, dist_prior)
+        if 'sigmap' not in par and 'sigmav' not in par:
+            prior_nosigma.add_parameter(par, dist_prior)
+        if 'Ps' not in par and ('sigmap' not in par and 'sigmav' not in par):
+            prior_noshot.add_parameter(par, dist_prior)
+        if 'Ps' not in par and 'lnbg' not in par and ('sigmap' not in par and 'sigmav' not in par):
+            prior_noshot_no_bi.add_parameter(par, dist_prior)
+
+
+print('Loaded prior into Nautilus with dimension',prior.dimensionality())
+print('Loaded prior into Nautilus with dimension',prior_noshot.dimensionality())
+print('Loaded prior into Nautilus with dimension',prior_noshot_no_bi.dimensionality())
+print('Loaded prior into Nautilus with dimension',prior_nosigma.dimensionality())
+print('Prior keys: ',prior.keys)
+print('Prior keys noshot: ',prior_noshot.keys)
+print('Prior keys noshot no bi: ',prior_noshot_no_bi.keys)
+print('Prior keys nosigma: ',prior_nosigma.keys)
+
+
+outfile_chain_path = 'cosmicjellyfish_eucid_spectro_wedges_nonuis_vs_camb'
+prior_to_use = prior_noshot_no_bi
+print("Loading prior with keys: ", prior_to_use.keys)
+
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore")
+    sampler = Sampler(prior_to_use, 
+                       loglike, 
+                       n_live=1000, 
+                       n_networks=4, 
+                       n_batch=256, pool=8, 
+                       pass_dict=False,
+                       filepath=outfile_chain_path+'.hdf5', 
+                          resume=True,
+                          likelihood_kwargs={'leg_flag': 'wedges', 
+                                             'prior': prior_to_use,
+                                             'cosmoFM_theory': cosmoFM_theory,
+                                             'cosmoFM_data': cosmoFM_data,
+                                             'data_obsPgg': obsPgg_data}
+                       )
+    sampler.run(verbose=True, discard_exploration=True)
+    log_z_all = sampler.evidence()
+print('Evidence:', log_z_all)
+print("Sampling posterior")
+points_all, log_w_all, log_l_all = sampler.posterior()
+
+
+sample_wghlkl = (np.vstack((points_all.T, np.exp(log_w_all), log_l_all)).T)
+
+
+outfile_new = outfile_chain_path
+print(f"Saving chain to text file {outfile_new}.txt")
+
+
+np.savetxt(outfile_new + '.txt', sample_wghlkl)
+
+
+maindir = './'
+
+
+cjelly_spectro_nonui = maindir + 'cosmicjellyfish_eucid_spectro_nonuis.txt'
+spectro_chain_nonui = fco.load_Nautilus_chains_from_txt(cjelly_spectro_nonui, 
+                                                      param_cols=parss_nonui)
+
+
+cjelly_spectro_nonui_vs_camb = maindir + 'cosmicjellyfish_eucid_spectro_wedges_nonuis_vs_camb.txt'
+spectro_chain_nonui_vs_camb = fco.load_Nautilus_chains_from_txt(cjelly_spectro_nonui_vs_camb, 
+                                                      param_cols=parss_nonui)
+
+
+euclid_sp_nonui = fco.make_triangle_plot(
+    chains=[
+        spectro_chain_nonui,
+        spectro_chain_nonui_vs_camb
+        ],
+    fishers=[
+        theory_fish_nonui,
+        #data_fish_nonui
+        ],
+    colors=[
+        'purple', 
+        #'red', 
+        'green', 
+        'orange'
+        ],
+    chain_labels=[
+        'Euclid MCMC GCsp no-nui symb',
+        'Euclid MCMC GCsp no-nui symb vs camb'
+        ],
+    fisher_labels=[
+        'Euclid Fisher GCsp no-nui symb',
+        #'Euclid Fisher GCsp no-nui camb',
+        ],
+    truth_values=truth_fish,
+    params=parss_nonui,
+    smooth=15,
+    bins=15
+)
+
+
+
+

0f39ff0cef223832ba2fb9187af0870dc8be6183
Santiago Casas <santiagocasas@gmail.com>
2025-10-05 00:02:03 +0200

    initial performance

--- cosmicfishpie/LSSsurvey/photo_cov.py
+++ cosmicfishpie/LSSsurvey/photo_cov.py
@@ -4,8 +4,8 @@
 This is the main engine for LSS Fisher Matrix.
 
 """
-import copy
 import datetime
+from collections import OrderedDict
 from itertools import product
 from time import time
 
@@ -22,7 +22,16 @@ pd.set_option("display.float_format", "{:.9E}".format)
 
 
 class PhotoCov:
-    def __init__(self, cosmopars, photopars, IApars, biaspars, fiducial_Cls=None):
+    def __init__(
+        self,
+        cosmopars,
+        photopars,
+        IApars,
+        biaspars,
+        fiducial_Cls=None,
+        enable_cache=True,
+        cache_size=32,
+    ):
         """Main class to obtain the ingredients for the Fisher matrix of a photometric probe
 
         Parameters
@@ -91,6 +100,19 @@ class PhotoCov:
         self.numbins_GCph = len(cfg.specs["z_bins_GCph"]) - 1
         self.ellipt_error = cfg.specs["ellipt_error"]
 
+        # Performance tuning options
+        self._enable_cache = enable_cache
+        self._cls_cache = OrderedDict()
+        self._cls_cache_max = max(1, cache_size)
+        # Precompute static noise vectors (used in getclsnoise)
+        self._wl_shape_noise = None
+        if self.numbins_WL > 0:
+            # sigma_e^2 / n_g
+            self._wl_shape_noise = (self.ellipt_error**2.0) / self.ngalbin_WL
+        self._gcph_shot_noise = None
+        if self.numbins_GCph > 0:
+            self._gcph_shot_noise = 1.0 / self.ngalbin_GCph
+
     def getcls(self, allpars):
         """Function to calculate the angular power spectrum
 
@@ -104,10 +126,25 @@ class PhotoCov:
         dict
             a dictionary with all the auto and cross correlation angular power spectra
         """
-        cosmopars = dict((k, allpars[k]) for k in self.cosmopars)
-        photopars = dict((k, allpars[k]) for k in self.photopars)
-        IApars = dict((k, allpars[k]) for k in self.IApars)
-        biaspars = dict((k, allpars[k]) for k in self.biaspars)
+        cosmopars = {k: allpars[k] for k in self.cosmopars}
+        photopars = {k: allpars[k] for k in self.photopars}
+        IApars = {k: allpars[k] for k in self.IApars}
+        biaspars = {k: allpars[k] for k in self.biaspars}
+
+        cache_key = None
+        if self._enable_cache:
+            # Immutable sorted tuple of all parameter key-value pairs across groups
+            cache_key = (
+                tuple(sorted(cosmopars.items()))
+                + tuple(sorted(photopars.items()))
+                + tuple(sorted(IApars.items()))
+                + tuple(sorted(biaspars.items()))
+            )
+            cached = self._cls_cache.get(cache_key)
+            if cached is not None:
+                # LRU update
+                self._cls_cache.move_to_end(cache_key)
+                return cached
         if cosmopars == self.cosmopars and self.fiducial_Cls is not None:
             cls = photo_obs.ComputeCls(
                 cosmopars, photopars, IApars, biaspars, fiducial_cosmo=self.fiducial_Cls.cosmo
@@ -117,7 +154,11 @@ class PhotoCov:
 
         cls.compute_all()
         LSScls = cls.result
-
+        if self._enable_cache and cache_key is not None:
+            self._cls_cache[cache_key] = LSScls
+            if len(self._cls_cache) > self._cls_cache_max:
+                # pop least recently used
+                self._cls_cache.popitem(last=False)
         return LSScls
 
     def getclsnoise(self, cls):
@@ -142,20 +183,16 @@ class PhotoCov:
 
             \\sigma_L = \\sigma_\\epsilon \\quad , \\quad \\sigma_G = 1
         """
-        noisy_cls = copy.deepcopy(cls)
-
-        for obs in self.observables:
-            if obs == "GCph":
-                for ind in self.binrange_GCph:
-                    noisy_cls[obs + " " + str(ind) + "x" + obs + " " + str(ind)] += (
-                        1.0 / self.ngalbin_GCph[ind - 1]
-                    )
-            elif obs == "WL":
-                for ind in self.binrange_WL:
-                    noisy_cls[obs + " " + str(ind) + "x" + obs + " " + str(ind)] += (
-                        self.ellipt_error**2.0
-                    ) / self.ngalbin_WL[ind - 1]
-
+        # Shallow copy: we reassign modified arrays so originals aren't altered.
+        noisy_cls = dict(cls)
+        if "GCph" in self.observables and self._gcph_shot_noise is not None:
+            for ind in self.binrange_GCph:
+                k = f"GCph {ind}xGCph {ind}"
+                noisy_cls[k] = noisy_cls[k] + self._gcph_shot_noise[ind - 1]
+        if "WL" in self.observables and self._wl_shape_noise is not None:
+            for ind in self.binrange_WL:
+                k = f"WL {ind}xWL {ind}"
+                noisy_cls[k] = noisy_cls[k] + self._wl_shape_noise[ind - 1]
         return noisy_cls
 
     def get_covmat(self, noisy_cls):
@@ -187,6 +224,9 @@ class PhotoCov:
                 for ind in range(self.numbins_GCph):
                     cols.append(o + " " + str(ind + 1))
 
+        # Precompute simple fsky factors (original code used sqrt(sqrt(fsky*fsky)) == sqrt(fsky))
+        fsky_factor = {o: np.sqrt(getattr(self, "fsky_" + o)) for o in self.observables}
+
         for ind, ell in enumerate(noisy_cls["ells"]):
             covdf = pd.DataFrame(index=cols, columns=cols)
             covdf = covdf.fillna(0.0).infer_objects(copy=False)
@@ -195,10 +235,9 @@ class PhotoCov:
                 for bin1, bin2 in product(
                     self.binrange[obs1], self.binrange[obs2]
                 ):  # MMmod: BEWARE!!! THIS IS VERY UGLY!
-                    covdf.at[obs1 + " " + str(bin1), obs2 + " " + str(bin2)] = noisy_cls[
-                        obs1 + " " + str(bin1) + "x" + obs2 + " " + str(bin2)
-                    ][ind] / np.sqrt(
-                        np.sqrt(getattr(self, "fsky_" + obs1) * getattr(self, "fsky_" + obs1))
+                    covdf.at[obs1 + " " + str(bin1), obs2 + " " + str(bin2)] = (
+                        noisy_cls[obs1 + " " + str(bin1) + "x" + obs2 + " " + str(bin2)][ind]
+                        / fsky_factor[obs1]
                     )
 
             covvec.append(covdf)
